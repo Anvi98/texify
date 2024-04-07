5e0b2d158f59e807c252a83cbb9df5f3bfed0bb2
Anvi Alex <epononanvialex@gmail.com>
2024-04-07 14:04:08 -0600

    Update settings.py

--- texify/settings.py
+++ texify/settings.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     TORCH_DEVICE: Optional[str] = None
     MAX_TOKENS: int = 768 # Will not work well above 768, since it was not trained with more
     MAX_IMAGE_SIZE: Dict = {"height": 420, "width": 420}
-    MODEL_CHECKPOINT: str = "vikp/texify"
+    MODEL_CHECKPOINT: str = "Anvi98/texify"
     BATCH_SIZE: int = 16 # Should use ~5GB of RAM
     DATA_DIR: str = "data"
     TEMPERATURE: float = 0.0 # Temperature for generation, 0.0 means greedy
